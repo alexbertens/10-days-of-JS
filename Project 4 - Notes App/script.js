b3d3919b42d2fdb38dbbf2f4b8fd59ca61c29633
Alex Bertens <alexbertens@Alexs-MacBook-2.local>
2021-07-18 13:50:20 -0600

    project 4 continue

--- Project 4 - Notes App/script.js
+++ Project 4 - Notes App/script.js
@@ -1,10 +1,18 @@
 const addBtn = document.getElementById('add');
 
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes) {
+    notes.forEach((note) => {
+        addNewNote(note);
+    });
+}
+
 addBtn.addEventListener('click', () => {
     addNewNote();
 });
 
-function addNewNote() {
+function addNewNote(text = "") {
     const note = document.createElement('div');
     note.classList.add('note');
 
@@ -15,8 +23,8 @@ function addNewNote() {
                 <button class="edit"><i class="fas fa-edit"></i></button>
                 <button class="delete"><i class="fas fa-trash-alt"></i></button>
             </div>
-            <div class="main hidden"></div>
-            <textarea></textarea>
+            <div class="main ${text ? "" : "hidden"}"></div>
+            <textarea class="main ${text ? "hidden" : ""}"></textarea>
         </div>
     
     `;
@@ -27,6 +35,9 @@ const deleteBtn = note.querySelector(".delete");
 const main = note.querySelector(".main");
 const textArea = note.querySelector("textarea");
 
+textArea.value = text;
+main.innerHTML = marked(text);
+
 editBtn.addEventListener("click", () => {
     main.classList.toggle("hidden");
     textArea.classList.toggle("hidden");
@@ -34,6 +45,8 @@ editBtn.addEventListener("click", () => {
 
 deleteBtn.addEventListener('click', () => {
     note.remove();
+
+    updateLS();
 });
 
 textArea.addEventListener("input", (e) => {
@@ -43,5 +56,17 @@ textArea.addEventListener("input", (e) => {
 });
 
     document.body.appendChild(note);
+}
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea');
+
+    const notes = [];
+
+    notesText.forEach((note) => {
+        notes.push(note.value);
+    });
+
+    localStorage.setItem('notes', JSON.stringify(notes));
 }
 
